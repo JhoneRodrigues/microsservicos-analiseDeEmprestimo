7bee1225fe3f74bdead9474f3279d87e95c55e81
JhoneRodrigues <jhonecosta30@gmail.com>
2024-05-13 16:16:10 -0300

    configure queues and exchanges

---
+++ proposta/src/main/java/com/jhone/propostaapp/config/RabbitMqConfiguration.java
@@ -0,0 +1,64 @@
+package com.jhone.propostaapp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMqConfiguration {
+
+    private final ConnectionFactory connectionFactory;
+
+    @Bean
+    public Queue criarFilaPropostaPendenteMsAnaliseDeCredito(){
+        return QueueBuilder.durable("proposta-pendente.ms-analise-credito").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaPendenteMsNotificacao(){
+        return QueueBuilder.durable("proposta-pendente.ms-notificacao").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsProposta(){
+        return QueueBuilder.durable("proposta-concluida.ms-proposta").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsNotificacao(){
+        return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin (ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializarAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public FanoutExchange criarFanoutExchangePropostaPendente(){
+        return ExchangeBuilder.fanoutExchange("proposta-pendente.ex").build();
+    }
+
+    @Bean
+    public Binding criarBidingPropostaPendenteMsAnaliseDeCredito(){
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsAnaliseDeCredito())
+                .to(criarFanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public Binding criarBidingPropostaPendenteMsNotificacao(){
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsNotificacao())
+                .to(criarFanoutExchangePropostaPendente());
+    }
+}
