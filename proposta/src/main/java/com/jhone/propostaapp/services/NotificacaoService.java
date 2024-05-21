36b6a35157603aeb67b0ac4eb410bdae882cb934
JhoneRodrigues <jhonecosta30@gmail.com>
2024-05-21 19:39:19 -0300

    refactor: implementing resilience

--- proposta/src/main/java/com/jhone/propostaapp/services/NotificacaoService.java
+++ proposta/src/main/java/com/jhone/propostaapp/services/NotificacaoService.java
@@ -1,6 +1,6 @@
 package com.jhone.propostaapp.services;
 
-import com.jhone.propostaapp.dtos.PropostaResponseDto;
+import com.jhone.propostaapp.entities.Proposta;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -11,7 +11,7 @@ public class NotificacaoService {
 
     private final RabbitTemplate rabbitTemplate;
 
-    public void notificar (PropostaResponseDto proposta, String exchange){
+    public void notificar (Proposta proposta, String exchange){
         rabbitTemplate.convertAndSend(exchange, "", proposta);
     }
 }
--- proposta/src/main/java/com/jhone/propostaapp/services/PropostaService.java
+++ proposta/src/main/java/com/jhone/propostaapp/services/PropostaService.java
@@ -5,7 +5,6 @@ import com.jhone.propostaapp.dtos.PropostaResponseDto;
 import com.jhone.propostaapp.entities.Proposta;
 import com.jhone.propostaapp.mapper.PropostaMapper;
 import com.jhone.propostaapp.repositories.PropostaRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +13,7 @@ import java.util.List;
 @Service
 public class PropostaService {
 
-    private PropostaRepository repository;
+    private PropostaRepository propostaRepository;
     private NotificacaoService notificacaoService;
     private String exchange;
 
@@ -22,21 +21,29 @@ public class PropostaService {
                             NotificacaoService notificacaoService, PropostaRepository repository) {
         this.exchange = exchange;
         this.notificacaoService = notificacaoService;
-        this.repository = repository;
+        this.propostaRepository = repository;
     }
 
     public List<PropostaResponseDto> obterProposta(){
-        return PropostaMapper.INSTANCE.convertListEntityToDto(repository.findAll());
+        return PropostaMapper.INSTANCE.convertListEntityToDto(propostaRepository.findAll());
     }
 
     public PropostaResponseDto criar (PropostaRequestDto request){
         Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(request);
-        repository.save(proposta);
+        propostaRepository.save(proposta);
 
-        var response = PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+        notificarRabbitMq(proposta);
 
-        notificacaoService.notificar(response,exchange);
+        return PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+    }
 
-        return response;
+    private void notificarRabbitMq(Proposta proposta){
+        try {
+            notificacaoService.notificar(proposta,exchange);
+        } catch (RuntimeException ex){
+            proposta.setIntegrada(false);
+            propostaRepository.save(proposta);
+        }
+
     }
 }
